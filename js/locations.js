cf416ab1aa675ca84141f8836a81b2f5fe69b428
Daniel Ruiz <108552382+RuizDann@users.noreply.github.com>
2023-10-24 20:43:08 -0700

    fetch data from all pages then loads into table

--- js/locations.js
+++ js/locations.js
@@ -1,84 +1,46 @@
-// API URL
-let api_url = 'https://rickandmortyapi.com/api/location?page=1';
-
-// Function to make an API request and display data
-async function getapi(url) {
+async function fetchData(url) {
   try {
     const response = await fetch(url);
-
-    if (response.ok) {
-      const data = await response.json();
-      show(data);
-    } else {
-      console.error('API request failed with status: ' + response.status);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
+    return await response.json();
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error fetching data:", error);
+    throw error;
   }
 }
 
-// Initial API request
-getapi(api_url);
-
-// Function to generate the HTML table
-function show(data) {
-  let table = `
-    <tr>
-        <th>Name</th>
-        <th>Type</th>
-        <th>Dimension</th>
-        <th>Residents</th>
-    </tr>`;
-
-  for (let location of data.results) {
-    for (let key in location) {
-      if (location[key] == '' || location[key] == null) {
-        location[key] = 'N/A';
-      }
+async function fetchAllData(url) {
+  let allLocations = [];
+  try {
+    while (url) {
+      const data = await fetchData(url);
+      allLocations = allLocations.concat(data.results);
+      console.log("allLocations:", allLocations);
+      url = data.info.next;
     }
 
-    table += `<tr>
-        <td>${location.name}</td>
-        <td>${location.type}</td>
-        <td>${location.dimension}</td>
-        <td>${location.residents.length}</td>
-    </tr>`;
+    displayDataInTable(allLocations);
+  } catch (error) {
+    console.error("Error fetching all data:", error);
   }
-
-  // Add navigation buttons
-  table += `<tr>
-        <td colspan="4">
-            <button id="prevBtn" onclick="previousPage()">Previous</button>
-            <button id="nextBtn" onclick="nextPage()">Next</button>
-        </td>
-    </tr>`;
-
-  document.getElementById('locations').innerHTML = table;
-  document.getElementById('locations').createCaption().innerHTML = 'Locations Table ' + parseInt(api_url.match(/\d+$/)[0]);
 }
 
-// Function to get the next page
-function nextPage() {
-  api_url = getNextPage(api_url);
-  getapi(api_url);
+function displayDataInTable(locations) {
+  const tableBody = document.querySelector("#locationsTable tbody");
+
+  locations.forEach((location) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>${location.name || "N/A"}</td>
+      <td>${location.type || "N/A"}</td>
+      <td>${location.dimension || "N/A"}</td>
+      <td>${location.residents.length || "N/A"}</td>
+    `;
+    tableBody.appendChild(row);
+  });
 }
 
-// Function to get the previous page
-function previousPage() {
-  api_url = getPreviousPage(api_url);
-  getapi(api_url);
-}
-
-// Helper functions for getting next and previous page URLs
-function getNextPage(url) {
-  const currentPage = parseInt(url.match(/\d+$/)[0]);
-  return url.replace(/(\d+)$/, currentPage + 1);
-}
-
-function getPreviousPage(url) {
-  const currentPage = parseInt(url.match(/\d+$/)[0]);
-  if (currentPage > 1) {
-    return url.replace(/(\d+)$/, currentPage - 1);
-  }
-  return url;
-}
+const apiUrl = "https://rickandmortyapi.com/api/location";
+fetchAllData(apiUrl);
