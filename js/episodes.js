29af88d3caed830aeda9a023994b599ead0ebb66
Daniel Ruiz <Ruizdaniel.e@outlook.com>
2023-11-16 23:01:18 -0800

    updated to match locations table

--- js/episodes.js
+++ js/episodes.js
@@ -1,86 +1,53 @@
-// API URL
-let api_url = 'https://rickandmortyapi.com/api/episode?page=1';
-
-// Function to make an API request and display data
-async function getapi(url) {
+// Function to make an API request and handle errors
+async function fetchData(url) {
   try {
     const response = await fetch(url);
 
     if (response.ok) {
-      const data = await response.json();
-      show(data);
+      return await response.json();
     } else {
-      console.error('API request failed with status: ' + response.status);
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error fetching data:", error);
+    throw error;
   }
 }
 
-// Initial API request
-getapi(api_url);
+// Function to fetch all data from paginated API
+async function fetchAllData(url) {
+  let allEpisodes = [];
 
-// Function to generate the HTML table
-function show(data) {
-  let table = `
-    <tr>
-        <th>Episode</th>
-        <th>Name</th>
-        <th>Air Date</th>
-        <th>Episode</th>
-        <th>Characters</th>
-    </tr>`;
-
-  for (let episode of data.results) {
-    for (let key in episode) {
-      if (episode[key] == '' || episode[key] == null) {
-        episode[key] = 'N/A';
-      }
+  try {
+    while (url) {
+      const data = await fetchData(url);
+      allEpisodes = allEpisodes.concat(data.results);
+      console.log("allEpisodes:", allEpisodes);
+      url = data.info.next;
     }
 
-    table += `<tr>
-        <td>${episode.episode}</td>
-        <td>${episode.name}</td>
-        <td>${episode.air_date}</td>
-        <td>${episode.episode}</td>
-        <td>${episode.characters.length}</td>
-    </tr>`;
+    displayDataInTable(allEpisodes);
+  } catch (error) {
+    console.error("Error fetching all data:", error);
   }
-
-  // Add navigation buttons
-  table += `<tr>
-        <td colspan="5">
-            <button id="prevBtn" onclick="previousPage()">Previous</button>
-            <button id="nextBtn" onclick="nextPage()">Next</button>
-        </td>
-    </tr>`;
-
-  document.getElementById('episodes').innerHTML = table;
-  document.getElementById('episodes').createCaption().innerHTML = 'Episodes Table ' + parseInt(api_url.match(/\d+$/)[0]);
 }
 
-// Function to get the next page
-function nextPage() {
-  api_url = getNextPage(api_url);
-  getapi(api_url);
+// Function to display data in a table
+function displayDataInTable(episodes) {
+  const tableBody = document.querySelector("#episodesTable tbody");
+
+  episodes.forEach((episode) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>${episode.name || "N/A"}</td>
+      <td>${episode.air_date || "N/A"}</td>
+      <td>${episode.episode || "N/A"}</td>
+      <td>${episode.characters.length || "N/A"}</td>
+    `;
+    tableBody.appendChild(row);
+  });
 }
 
-// Function to get the previous page
-function previousPage() {
-  api_url = getPreviousPage(api_url);
-  getapi(api_url);
-}
-
-// Helper functions for getting next and previous page URLs
-function getNextPage(url) {
-  const currentPage = parseInt(url.match(/\d+$/)[0]);
-  return url.replace(/(\d+)$/, currentPage + 1);
-}
-
-function getPreviousPage(url) {
-  const currentPage = parseInt(url.match(/\d+$/)[0]);
-  if (currentPage > 1) {
-    return url.replace(/(\d+)$/, currentPage - 1);
-  }
-  return url;
-}
+// Initial API URL for episodes
+const apiUrlEpisodes = "https://rickandmortyapi.com/api/episode";
+fetchAllData(apiUrlEpisodes);
